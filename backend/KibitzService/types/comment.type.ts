af7cee3a678c49f24f8a62a832835ae212afbef4
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-07-03 10:03:52 +0300

    added kib and parent comment to comment type

--- backend/KibitzService/types/comment.type.ts
+++ backend/KibitzService/types/comment.type.ts
@@ -4,6 +4,8 @@ export type CommentType = {
     _id?: Types.ObjectId;
     commentName: string;
     shmoozerId: Types.ObjectId;
+    kibId: Types.ObjectId;
+    parentCommentId: Types.ObjectId;
     text: string;
     media?: string;
     createdAt?: Date;
