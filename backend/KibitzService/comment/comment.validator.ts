9d1f7fc24a4aa542500c875b126b4373f927f9c9
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-07-03 10:04:19 +0300

    added kib and parent comment to comment validator

--- backend/KibitzService/comment/comment.validator.ts
+++ backend/KibitzService/comment/comment.validator.ts
@@ -3,6 +3,8 @@ import Joi from 'joi';
 export const commentBodySchema = Joi.object({
     commentName: Joi.string().required(),
     shmoozerId: Joi.string().hex().length(24).required(), //hex and length to make it fit with the ObjectId type in MongoDB
+    kibId: Joi.string().hex().length(24).required(),
+    parentCommentId: Joi.string().hex().length(24).required(),
     text: Joi.string().max(200).required(),
     media: Joi.string().uri().optional(),
     createdAt: Joi.date().optional(),
