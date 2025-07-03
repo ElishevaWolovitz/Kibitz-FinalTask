e0a7f9fd0e270e12119400439ad48b3b222a9c45
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-07-03 15:35:07 +0300

    made parentCommentId optional

--- backend/KibitzService/src/comment/comment.validator.ts
+++ backend/KibitzService/src/comment/comment.validator.ts
@@ -4,7 +4,7 @@ export const commentBodySchema = Joi.object({
     commentName: Joi.string().required(),
     shmoozerId: Joi.string().hex().length(24).required(), //hex and length to make it fit with the ObjectId type in MongoDB
     kibId: Joi.string().hex().length(24).required(),
-    parentCommentId: Joi.string().hex().length(24).required(),
+    parentCommentId: Joi.string().hex().length(24),
     text: Joi.string().max(200).required(),
     media: Joi.string().uri().optional(),
     createdAt: Joi.date().optional(),
