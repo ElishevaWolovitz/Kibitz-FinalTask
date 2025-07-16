94bd98cbcef00235a81f9b3294fee9791eab20b6
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-07-16 10:13:27 +0300

    add use query tanstack devtool

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import ShmoozerNameProvider from './contexts/ShmoozerNameContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 
 const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
       <ShmoozerNameProvider>
         <QueryClientProvider client={queryClient}>
           <App />
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </ShmoozerNameProvider>
     </BrowserRouter>
